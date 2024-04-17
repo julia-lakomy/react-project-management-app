37afb3615eff95d8d8b028ebac1d51e5d455ade7
julia-lakomy <julialakomydev@gmail.com>
2024-04-17 14:27:00 +0100

    Add SelectedProject component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
@@ -29,7 +30,7 @@ function App() {
 
       return {
         ...prevState,
-        selectedProjectId: undefined, 
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -44,7 +45,20 @@ function App() {
     });
   }
 
-  let content;
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject}></SelectedProject>;
 
   if (projectsState.selectedProjectId === null) {
     content = (
@@ -66,8 +80,8 @@ function App() {
       <ProjectsSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
       ></ProjectsSidebar>
-
       {content}
     </main>
   );
