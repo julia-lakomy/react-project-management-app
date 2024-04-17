7f8f2385a60e1e242a7e037be06299c064d4d63d
julia-lakomy <julialakomydev@gmail.com>
2024-04-17 13:38:00 +0100

    Add state management and improve input component

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,23 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -10,17 +27,21 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md">
+          <button
+            className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
 
       <div>
-        <Input label="Title"></Input>
-        <Input label="Description" isTextArea></Input>
-        <Input label="Due date"></Input>
+        <Input ref={title} label="Title"></Input>
+        <Input ref={description} label="Description" isTextArea></Input>
+        <Input ref={dueDate} label="Due date"></Input>
       </div>
+
     </div>
   );
 }
