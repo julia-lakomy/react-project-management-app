412371284ae177f348d94ac4274ecaa03eaf17f9
julia-lakomy <julialakomydev@gmail.com>
2024-04-17 11:20:45 +0200

    Add ProjectsSidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">React Project Manager</h1>
-    </>
+    <main>
+      <ProjectsSidebar></ProjectsSidebar>
+    </main>
   );
 }
 
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,14 @@
+export default function ProjectsSidebar(){
+
+    return(
+        <aside>
+            <h2>Your Projects</h2>
+            <div>
+                <button>
+                   + Add project
+                </button>
+            </div>
+            <ul>Project element</ul>
+        </aside>
+    )
+}
